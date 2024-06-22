840e58dc1dbeb2fc218453f4d71c22b3a43d7d19
crap1nyk <nv06645@gmail.com>
2024-06-22 09:45:05 +0530

    REVEAL TEXT

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,17 +1,25 @@
-import './Hero.scss'
-import iPhone from '../../assets/iPhone-png.png'
-const Hero = ({title}) => {
-    return <div className="wrapper">
-        <div className="title">
-        <img src={title}/>
-            <div className="desc">
-                where <span style={{fontWeight:"bolder"}}>AI </span> meets <span style={{fontStyle:"italic"}}>fashion</span>
-            </div>
+import './Hero.scss';
+import iPhone from '../../assets/iPhone-png.png';
+
+const Hero = ({ title }) => {
+  return (
+    <div className="wrapper">
+      <div className="title">
+        <img src={title} alt="Title" />
+        <div className="desc-container">
+          <div className="desc-line desc-line-1">
+            WEAR <span>YOUR CREATIVITY</span>.<span style={{ fontStyle: 'italic' }}>EXPRESS YOURSELF</span>
+          </div>
+          <div className="desc-line desc-line-2">
+            ROOKUS. WHERE AI MEETS FASHION
+          </div>
         </div>
-        <div className="app">
-           <img src={iPhone} alt="" srcset="" />
-        </div>
+      </div>
+      <div className="app">
+        <img src={iPhone} alt="iPhone" />
+      </div>
     </div>
-}
+  );
+};
 
 export default Hero;
