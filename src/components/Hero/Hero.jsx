9cb6bcab7c8d3b9fbb8ca0acfd0b11f4edb5546c
Shubhraneel Gupta <btech10583.22@bitmesra.ac.in>
2024-06-24 00:37:37 +0530

    Edits

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,11 +1,13 @@
 import './Hero.scss';
 import iPhone from '../../assets/DEMO-removebg.png';
+import {motion} from 'framer-motion'
 
 const Hero = ({ title }) => {
   return (
     <div className="wrapper">
       <div className="title">
-        <img src={title} alt="Title" />
+            <img src={title} alt="" srcset="" />    
+
         <div className="desc">
             WEAR <span>YOUR CREATIVITY</span>.<span style={{ fontStyle: 'italic' }}>EXPRESS YOURSELF</span>
             <br/>ROOKUS. WHERE AI MEETS FASHION
