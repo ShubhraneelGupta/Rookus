b7c2494bec2bb1c681ea4bc0f89bf7b4a89c614d
crap1nyk <nv06645@gmail.com>
2024-06-24 00:55:20 +0530

    Phew

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,17 +7,30 @@ const Hero = ({ title }) => {
     <div className="wrapper">
       <div className="title">
             <img src={title} alt="" srcset="" />    
-
-        <div className="desc">
-            WEAR <span>YOUR CREATIVITY</span>.<span style={{ fontStyle: 'italic' }}>EXPRESS YOURSELF</span>
-            <br/>ROOKUS. WHERE AI MEETS FASHION
+            <div className="desc-container">  
+            <motion.div
+            className="desc-line desc-line-1"
+            initial={{ width: 0, opacity: 0 }}
+            animate={{ width: '100%', opacity: 1 }}
+            transition={{ duration: 3, ease: 'easeInOut' }}
+          >
+            <span>WEAR YOUR CREATIVITY</span>.<span style={{ fontStyle: 'italic' }}>EXPRESS YOURSELF</span>
+          </motion.div>
+          <motion.div
+            className="desc-line desc-line-2"
+            initial={{ width: 0, opacity: 0 }}
+            animate={{ width: '100%', opacity: 1 }}
+            transition={{ duration: 3, ease: 'easeInOut', delay: 3 }}
+          >
+            <strong>ROOKUS,</strong> WHERE AI MEETS FASHION
+          </motion.div>
         </div>
         <div className="button-container">
           <motion.button
             className="waitlist-button"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1.5, ease: 'easeInOut', delay: 9 }}
+            transition={{ duration: 1.5, ease: 'easeInOut', delay: 6 }}
           >
             Waitlist
           </motion.button>
@@ -25,7 +38,7 @@ const Hero = ({ title }) => {
             className="demo-button"
             initial={{ opacity: 0, y: 20 }}
             animate={{ opacity: 1, y: 0 }}
-            transition={{ duration: 1.5, ease: 'easeInOut', delay: 9.5 }}
+            transition={{ duration: 1.5, ease: 'easeInOut', delay: 6.5 }}
           >
             Try Demo
           </motion.button>
