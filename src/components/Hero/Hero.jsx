7d3f6a28e9344b13bf0b09b945c753fe81174e1e
crap1nyk <nv06645@gmail.com>
2024-06-24 00:46:22 +0530

    merge conflicts

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,27 +1,64 @@
 import './Hero.scss';
+import { motion } from 'framer-motion';
 import iPhone from '../../assets/DEMO-removebg.png';
 
 const Hero = ({ title }) => {
   return (
     <div className="wrapper">
       <div className="title">
-        <img src={title} alt="Title" />
+      <img src={title} alt="" srcset="" />    
         <div className="desc-container">
-          <div className="desc-line desc-line-1">
-            WEAR <span>YOUR CREATIVITY</span>.<span style={{ fontStyle: 'italic' }}>EXPRESS YOURSELF</span>
-          </div>
-          <div className="desc-line desc-line-2">
-            ROOKUS. WHERE AI MEETS FASHION
-          </div>
+          <motion.div
+            className="desc-line desc-line-1"
+            initial={{ width: 0, opacity: 0 }}
+            animate={{ width: '100%', opacity: 1 }}
+            transition={{ duration: 3, ease: 'easeInOut' }}
+          >
+            <span>WEAR YOUR CREATIVITY</span>.<span style={{ fontStyle: 'italic' }}>EXPRESS YOURSELF</span>
+          </motion.div>
+          <motion.div
+            className="desc-line desc-line-2"
+            initial={{ width: 0, opacity: 0 }}
+            animate={{ width: '100%', opacity: 1 }}
+            transition={{ duration: 3, ease: 'easeInOut', delay: 3 }}
+          >
+            <strong>ROOKUS,</strong> WHERE AI MEETS FASHION
+          </motion.div>
+          <motion.div
+            className="desc-line desc-line-3"
+            initial={{ width: 0, opacity: 0 }}
+            animate={{ width: '100%', opacity: 1 }}
+            transition={{ duration: 3, ease: 'easeInOut', delay: 6 }}
+          >
+            
+          </motion.div>
         </div>
         <div className="button-container">
-          <button className="waitlist-button">Waitlist</button>
-          <button className="demo-button">Try Demo</button>
+          <motion.button
+            className="waitlist-button"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.5, ease: 'easeInOut', delay: 9 }}
+          >
+            Waitlist
+          </motion.button>
+          <motion.button
+            className="demo-button"
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 1.5, ease: 'easeInOut', delay: 9.5 }}
+          >
+            Try Demo
+          </motion.button>
         </div>
       </div>
       <div className="app">
         <img src={iPhone} alt="iPhone" />
+        
       </div>
+      <footer className="footer">
+        <p>&copy; 2024 Rookus. All rights reserved.</p>
+      </footer>
     </div>
   );
 };
