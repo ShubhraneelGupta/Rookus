68ce1620a5827f6f3e08019556ce24d842e58321
crap1nyk <nv06645@gmail.com>
2024-06-24 10:16:40 +0530

    contact form

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Hero from './components/Hero/Hero'
 import logo from './assets/rookus-logo.png'
 import title from './assets/Rookus-title.png'
 import Footer from './components/Footer/Footer'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   const navItems = ["Home", "Team", "Contact", "Features"]
   return<>
     <Header logoSRC={logo} navItems={navItems}/>
     <Hero title={title}/>
+    <Contact />
     <Footer/>
   </>
 }
